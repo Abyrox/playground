ab5ec8f510a89d4aeaea2a962ef50731a11ccaf3
Abyrox <abhishekgupta04@hotmail.com>
2018-04-05 16:13:49 -0700

    added outside

---
+++ helloWorld.java
@@ -0,0 +1,15 @@
+package aby_playground;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			for (int i=0;i<3;i++)
+			{
+				System.out.println("The number is - " + i);
+			}
+			System.out.println("The final number is - 3" );
+	}
+
+}
+
